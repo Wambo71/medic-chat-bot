0f20bd4643c035e8cf5afd52d0093003b3413b62
Wambui <wambuikaranja78@gmail.com>
2026-01-26 20:23:13 +0300

    creates and updates app server and webhook js

---
+++ routes/webhook.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { receiveMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+  return res.sendStatus(403);
+});
+
+router.post("/", receiveMessage);
+
+export default router;

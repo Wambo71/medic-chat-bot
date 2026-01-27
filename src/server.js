dad2b6cac2c7d5b0ef036a9fea4a0ac3a2beb3d1
Wambui <wambuikaranja78@gmail.com>
2026-01-27 19:28:49 +0300

    creates the database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
+dotenv.config();
+connectDB();
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Clinic bot running on port ${PORT}`);
+  console.log(`🏥 Clinic bot running on port ${PORT}`);
 });
